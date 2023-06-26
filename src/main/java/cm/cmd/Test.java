0cef935701bb8a158b460018f2afa72411b8ee70
elysetonke <elyse.tonke@gmail.com>
2023-06-26 12:56:48 -0400

    ajout boucle for

--- src/main/java/cm/cmd/Test.java
+++ src/main/java/cm/cmd/Test.java
@@ -5,6 +5,9 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("Bonjour le Labo");
+        for (int i = 0; i < 5; i++){
+            System.out.println(i+ ": Vous pouvez compter sur moi");
+        }
 
     }
 }
